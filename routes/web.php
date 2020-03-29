b4c1326c077e901dfbb0201755ba94defad3cfc1
xinghuifeng <2574047739@qq.com>
2020-03-29 19:31:45 +0800

    修改web.php

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,23 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::prefix('/visitor')->group(function(){
+	Route::get('/create',"Admin\VisitorController@create");
+	Route::post('/score',"Admin\VisitorController@score");
+	Route::get('/index',"Admin\VisitorController@index");
+	Route::get('/edit/{id}',"Admin\VisitorController@edit");
+	Route::get('/destroy/{id}',"Admin\VisitorController@destroy");
+	Route::post('/update/{id}',"Admin\VisitorController@update");
+});
+Route::prefix('/saleman')->group(function(){
+	Route::get('/create',"Admin\SalemanController@create");
+	Route::post('/score',"Admin\SalemanController@score");
+	Route::get('/index',"Admin\SalemanController@index");
+	Route::get('/edit/{id}',"Admin\SalemanController@edit");
+	Route::get('/destroy/{id}',"Admin\SalemanController@destroy");
+	Route::post('/update/{id}',"Admin\SalemanController@update");
+
 });
