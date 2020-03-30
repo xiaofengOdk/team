5b74cccaeb6128f0a265c7ccc274da2c088df36b
liuxinyue <1479450158@qq.com>
2020-03-30 11:45:29 +0800

    修改5

---
+++ resources/views/customer/create.blade.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8"> 
+	<title>客户添加</title>
+	<link rel="stylesheet" href="https://cdn.staticfile.org/twitter-bootstrap/3.3.7/css/bootstrap.min.css">  
+	<script src="https://cdn.staticfile.org/jquery/2.1.1/jquery.min.js"></script>
+	<script src="https://cdn.staticfile.org/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+<center><h2>客户添加<a style="float:right" href="{{url('/customer/index')}}" class="btn btn-default">列表</a></h2></center><hr/>
+<!-- @if ($errors->any()) 
+<div class="alert alert-danger">
+ <ul>
+ 	@foreach ($errors->all() as $error) 
+ 		<li>{{ $error }}</li>
+ 	@endforeach
+ </ul>
+</div>
+@endif -->
+
+<form action="{{url('/customer/store')}}" method="post" class="form-horizontal" role="form" >
+@csrf
+	<div class="form-group">
+		<label for="firstname" class="col-sm-2 control-label">客户名字</label>
+		<div class="col-sm-8">
+			<input type="text" class="form-control" id="firstname" name="c_name" placeholder="客户名字">
+			<b style="color:red">{{$errors->first('c_name')}}</b>
+		</div>
+	</div>
+	
+	<div class="form-group">
+		<label for="lastname" class="col-sm-2 control-label">客户级别</label>
+		<div class="col-sm-6">
+			<select class="form-control" name="c_grade">
+					<option value="">请选择</option>
+					
+					<option value="普通客户">普通客户</option>
+					<option value="VIP客户">VIP客户</option>
+					<option value="SVIP客户">SVIP客户</option>
+					
+			</select>
+		</div>
+	</div>
+	<div class="form-group">
+		<label for="firstname" class="col-sm-2 control-label">客户来源</label>
+		<div class="col-sm-8">
+			<input type="text" class="form-control" id="firstname" name="c_from" placeholder="客户来源">
+			<b style="color:red">{{$errors->first('c_from')}}</b>
+		</div>
+	</div>
+		
+	<div class="form-group">
+		<label for="lastname" class="col-sm-2 control-label">业务员</label>
+		<div class="col-sm-6">
+			<select class="form-control" name="c_from">
+					<option value="">请选择</option>
+					
+					<option value=""></option>
+					
+			</select>
+		</div>
+	</div>
+	<div class="form-group">
+		<label for="firstname" class="col-sm-2 control-label">客户电话</label>
+		<div class="col-sm-8">
+			<input type="text" class="form-control" id="firstname" name="c_tel" placeholder="请输入客户电话">
+			<b style="color:red">{{$errors->first('c_tel')}}</b>
+		</div>
+	</div>
+	<div class="form-group">
+		<label for="firstname" class="col-sm-2 control-label">客户手机</label>
+		<div class="col-sm-8">
+			<input type="text" class="form-control" id="firstname" name="c_ptel" placeholder="请输入客户手机">
+			<b style="color:red">{{$errors->first('c_ptel')}}</b>
+		</div>
+	</div>
+	<div class="form-group">
+		<div class="col-sm-offset-2 col-sm-10">
+			<button type="submit" class="btn btn-default">添加</button>
+		</div>
+	</div>
+</form>
+
+</body>
+</html>
+
+ 
---
+++ resources/views/customer/index.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>客户信息列表</title>
+	<link rel="stylesheet" href="https://cdn.staticfile.org/twitter-bootstrap/3.3.7/css/bootstrap.min.css">  
+	<script src="https://cdn.staticfile.org/jquery/2.1.1/jquery.min.js"></script>
+	<script src="https://cdn.staticfile.org/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+<center><h2>客户信息列表<a style="float:right" href="{{url('/customer/create')}}" class="btn btn-default">添加</a></h2></center><hr/>
+<div class="table-responsive">
+	
+	<table class="table">
+	
+		<thead>
+			<tr>
+				<th>id</th>
+				<th>客户名字</th>
+				<th>客户级别</th>
+				<th>客户来源</th>
+				<th>业务员</th>
+				<th>电话</th>
+				<th>手机</th>
+				<th>操作</th>
+			</tr>
+		</thead>
+		@foreach($customer as $v)
+		<tbody>
+			<tr>
+				<td>{{$v->c_id}}</td>
+				<td>{{$v->c_name}}</td>
+				<td>{{$v->c_grade}}</td>
+				<td>{{$v->c_from}}</td>
+				<td>{{$v->s_id}}</td>
+				<td>{{$v->c_tel}}</td>
+				<td>{{$v->c_ptel}}</td>
+				<td>
+					<a href="{{url('/customer/edit/'.$v->c_id)}}" class="btn btn-success">编辑</a>|
+					<a href="{{url('/customer/destroy/'.$v->c_id)}}" class="btn btn-danger">删除</a>
+				</td>
+			</tr>
+		@endforeach
+		<tr><td colspan=“6>{{$customer->links()}}</td></tr>
+	
+		</tbody>
+</table>
+</div>  	
+
+</body>
+</html>
