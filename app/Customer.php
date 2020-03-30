da27452671ba07e2a65e4e29b6464060bc5037e0
liuxinyue <1479450158@qq.com>
2020-03-30 11:43:49 +0800

    修改3

---
+++ app/Customer.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Customer extends Model
+{
+    // 指定表名
+    protected $table='customer';
+    protected $primaryKey='c_id';
+    public $timestamps=false;
+    // 黑名单
+    protected $guarded=[];
+}
