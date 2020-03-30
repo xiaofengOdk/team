277833fea66073d4ac982246d5fbd97fb4e7b1cf
xinghuifeng <2574047739@qq.com>
2020-03-30 15:29:10 +0800

    优化了

---
+++ app/Http/Controllers/Admin/IndexController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Saleman;
+use Illuminate\validation\Rule;
+class IndexController extends Controller
+{
+	public function index(){
+		return view('visitor.indexzhongxin');
+	}
+}
