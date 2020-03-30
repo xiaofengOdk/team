6df4329f4a927366d8c7878fe4027db51f85c13e
xinghuifeng <2574047739@qq.com>
2020-03-30 14:51:11 +0800

    修改了index.blade.php

---
+++ resources/views/visitor/indexzhongxin.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8"> 
+	<title>Bootstrap 实例 - 边框表格</title>
+	<link rel="stylesheet" href="https://cdn.staticfile.org/twitter-bootstrap/3.3.7/css/bootstrap.min.css">  
+	<script src="https://cdn.staticfile.org/jquery/2.1.1/jquery.min.js"></script>
+	<script src="https://cdn.staticfile.org/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<table class="table table-bordered"  width="200px" height="200px">
+	<h1 align="center">合作中心</h1>
+	<thead>
+		<tr >
+			<th style="height: 80px;font-size: 20px;background-color:pink;"><a href="{{'visitor/index'}}">拜访会议</a></th>
+			<th style="font-size: 20px;background-color:pink;"><a href="{{'customer/index'}}">顾客</a></th>
+			<th style="font-size: 20px;background-color:pink;"><a href="{{'saleman/index'}}">业务员</a></th>
+			<th style="font-size: 20px;background-color:pink;"><a href="{{'admin/index'}}">Admin</a></th>
+		</tr>
+	</thead>
+</table>
+</body>
+</html>
