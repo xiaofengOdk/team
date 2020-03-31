f61902ad7f717c9dd31f0b9440acf1cb211cd5b9
xinghuifeng <2574047739@qq.com>
2020-03-31 08:21:02 +0800

    修改

--- resources/views/saleman/indexpage.blade.php
+++ resources/views/saleman/indexpage.blade.php
@@ -3,8 +3,8 @@
 		<td>{{$v->s_id}}</td>
 		<td>{{$v->s_name}}</td>
 		<td>{{$v->s_sex=='1'?"男":"女"}}</td>
-		<td>{{$v->tel}}</td>
-		<td>{{$v->v_ptel}}</td>
+		<td>{{$v->s_tel}}</td>
+		<td>{{$v->s_ptel}}</td>
 		<td>
 			<a href="{{url('saleman/edit/'.$v->s_id)}}" class="btn btn-success">编辑</a>
 			<a href="{{url('saleman/destroy/'.$v->s_id)}}" class="btn btn-success">删除</a>
